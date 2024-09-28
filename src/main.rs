f072f4af4a0e2142f2a87938bffe74edd0cdd6f8
h-hafezi <h.hafezi2000@gmail.com>
2024-09-29 03:23:51 +0330

    add size println for bls12-381

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ pub mod schnorr_signature;
 pub mod blind_schnorr_signature;
 pub mod fde;
 
+use std::mem;
+
 fn main() {
-    println!("Hello, world!");
+    println!("BLS group element size: {} bytes", mem::size_of::<ark_bls12_381::g1::G1Affine>());
+    println!("BLS scalar field element size: {} bytes", mem::size_of::<ark_bls12_381::Fr>());
 }
